41d785e5d92cd462402b9f83ee40724b061fc5dc
Hiimtroy <61811480+huythinhta@users.noreply.github.com>
2020-12-10 22:16:43 +0700

    fix smt

--- src/main/java/pet/petshop/controller/ScheduleController.java
+++ src/main/java/pet/petshop/controller/ScheduleController.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pet.petshop.entity.Blog;
 import pet.petshop.entity.Schedule;
 import pet.petshop.entity.Services;
+import pet.petshop.entity.User;
 import pet.petshop.service.ScheduleService;
 import pet.petshop.service.ServiceServices;
 
@@ -23,8 +26,9 @@ public class ScheduleController {
 	private ServiceServices ss;
 	@Autowired
 	private ScheduleService scs;
+	
 	@RequestMapping("/confirmservice/{id}")
-	public String confirmservice(ModelMap model,@PathVariable(name = "id") Integer id) {
+	public String confirmservice(ModelMap model,HttpSession session,@PathVariable(name = "id") Integer id) {
 		Services service= ss.get(id);
 		Schedule sch = new Schedule();
 		model.addAttribute("schedule",sch);
@@ -34,14 +38,13 @@ public class ScheduleController {
 	
 	@RequestMapping("/order")
 	public String order(@ModelAttribute("schedule") Schedule sch) throws ParseException {
-		String pattern = "yyyy-MM-dd";
-		SimpleDateFormat format = new SimpleDateFormat(pattern);
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = new Date();
 		String now = format.format(date);
 		String datecheckin = format.format(sch.getDatacheckin());
-		
-	//	Schedule schedule = new Schedule(sch.getIdservice(), sch.getIduser(), format.parse(now), format.parse(source), sch.getNote());
-	//	scs.save(schedule);
-		return "index";
+		Schedule schedule = new Schedule(sch.getIdservice(), sch.getIduser(), format.parse(now), format.parse(datecheckin), sch.getNote());
+		scs.save(schedule);
+		return "/";
 	}
 }
